7ade9bac8931c083a98a1e7166017aaef1ca195e
leodayvison <leonardodayvison235@gmail.com>
2025-09-24 00:57:51 -0300

    add: funcoes de ler json, validar dispositivo e alguns callbacks do mqtt

--- validador/validador.py
+++ validador/validador.py
@@ -1,5 +1,8 @@
+import json
+import paho.mqtt.client as mqtt
+
 # variaveis universais
-CHAVE = 1
+KEY = 1
 DISPOSITIVOS_VALIDOS = ["dev1", "dev2", "dev3"]
 
 # decripta a cifra de cesar pela tabela ascii
@@ -10,3 +13,32 @@ def caesar_decrypt(message, key):
         c = chr(aux)
         res += c
     return res
+
+# lê a mensagem recebida e transforma em obj
+def read_json(s):
+    res = json.loads(s)
+    return res
+
+# verifica se o dispositivo ta na lista de dispositivos validos
+def device_validator(msg):
+    if msg['device_id'] not in DISPOSITIVOS_VALIDOS:
+        return False
+    return True
+
+
+# FUNCOES DO MQTT
+def on_connect(client, userdata, flags, reason_code, properties):
+    print(f"Conectado com result code {reason_code}")
+    client.subscribe("$SYS/#")
+
+def on_message(client, userdata, msg):
+    try:
+        decoded_message = caesar_decrypt(msg, KEY)
+        decoded_dict = read_json(decoded_message)
+    except Exception as e:
+        print("Não foi possível descriptografar:", e)
+        
+    if device_validator(decoded_dict):
+        pass #TODO guardar os dados
+    else: 
+        print("Dispositivo não identificado.")
